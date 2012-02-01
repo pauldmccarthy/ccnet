26da178a0cf682e3c7fba31711001984709f44f3
Paul McCarthy <pauld.mccarthy@gmail.com>
2012-02-02 10:45:24 +1300

    repimg appears to work

--- repimg.c
+++ repimg.c
@@ -36,21 +36,39 @@ static void _parse_rep(char *rep, args_t *args) {
 
   double  from;
   double  to;
+  char   *copy;
   char   *toptr;
 
-  if (args->nreps >= MAX_REPLACEMENTS);
+  copy = NULL;
 
-  toptr = strchr(rep, ',');
+  if (args->nreps >= MAX_REPLACEMENTS) goto fail;
 
-  if (toptr         == NULL) return;
-  if (strlen(toptr) == 0)    return;
+  copy = malloc(strlen(rep)+1);
+  if (copy == NULL) goto fail;
+  
+  strcpy(copy, rep);
+  toptr = strchr(copy, ',');
 
-  from = atof(rep);
-  to   = atof(toptr+1);
+  if (toptr == NULL) goto fail;
+
+  *toptr = '\0';
+  toptr ++;
+
+  if (strlen(copy)  == 0) goto fail;
+  if (strlen(toptr) == 0) goto fail;
+
+  from = atof(copy);
+  to   = atof(toptr);
 
   args->from[args->nreps] = from;
   args->to  [args->nreps] = to;
   args->nreps++;
+
+  free(copy);
+  return;
+  
+fail:
+  if (copy != NULL) free(copy);
 }
 
 static error_t _parse_opt(int key, char *arg, struct argp_state *state) {
@@ -77,12 +95,26 @@ static error_t _parse_opt(int key, char *arg, struct argp_state *state) {
   return 0;
 }
 
+/**
+ * Replaces values from the 'from' list with values from the 'to' list,
+ * in the given image.
+ */
+static void _replace(
+  dsr_t   *hdr,   /**< image header                     */
+  uint8_t *img,   /**< image data                       */
+  double  *from,  /**< list of values to replace        */
+  double  *to,    /**< corresponding replacement values */
+  uint16_t nreps  /**< length of from/to lists          */
+);
+
 int main(int argc, char *argv[]) {
 
   dsr_t       hdr;
   uint8_t    *img;
   args_t      args;
   struct argp argp = {options, _parse_opt, "INPUT OUTPUT", doc};
+
+  memset(&args, 0, sizeof(args));
 
   startup("repimg", argc, argv, &argp, &args);
 
@@ -91,7 +123,7 @@ int main(int argc, char *argv[]) {
     goto fail;
   }
 
-  
+  _replace(&hdr, img, args.from, args.to, args.nreps);
 
   if (analyze_write_hdr(args.output, &hdr)) {
     printf("error writing header %s\n", args.output);
@@ -107,4 +139,29 @@ int main(int argc, char *argv[]) {
   
 fail:
   return 1;
+}
+
+void _replace(
+  dsr_t *hdr, uint8_t *img, double *from, double *to, uint16_t nreps) {
+
+  uint64_t i;
+  uint64_t j;
+  uint32_t nvals;
+  double   val;
+
+  nvals = analyze_num_vals(hdr);
+
+  for (i = 0; i < nvals; i++) {
+
+    val = analyze_read_by_idx(hdr, img, i);
+
+    for (j = 0; j < nreps; j++) {
+
+      if (val == from[j]) {
+
+        analyze_write_by_idx(hdr, img, i, to[j]);
+        break;
+      }
+    }
+  }
 }

c3ba5823c74c893116fb9d0bd3142c2a7917ab8d
Paul McCarthy <pauld.mccarthy@gmail.com>
2012-05-01 10:41:49 +1200

    bugfixes/improvements to countimg

--- countimg.c
+++ countimg.c
@@ -5,6 +5,7 @@
  * Author: Paul McCarthy <pauld.mccarthy@gmail.com>
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -21,6 +22,7 @@ typedef struct _args {
   double   threshold;
   uint8_t  lessthan;
   uint8_t  absolute;
+  uint8_t  printidx;
 } args_t;
 
 
@@ -32,6 +34,7 @@ static struct argp_option options[] = {
   {"threshold", 't', "FLOAT", 0, "threshold"},
   {"lessthan",  'l',  NULL,   0, "count values <= threshold, rather than >="},
   {"absolute",  'a',  NULL,   0, "Use absolute values for comparison"},
+  {"printidx",  'p',  NULL,   0, "Print indices of voxels passing the test"},
   {0}
 };
 
@@ -43,7 +46,9 @@ static error_t _parse_opt(int key, char *arg, struct argp_state *state) {
   switch (key) {
 
     case 't': args->threshold = atof(arg); break;
+    case 'l': args->lessthan  = 0xFF;      break;
     case 'a': args->absolute  = 0xFF;      break;
+    case 'p': args->printidx  = 0xFF;      break;
 
     case ARGP_KEY_ARG:
       if (state->arg_num == 0) args->input = arg;
@@ -64,17 +69,22 @@ int main(int argc, char *argv[]) {
 
   dsr_t       hdr;
   uint8_t    *data;
-  double      threshold;
   double      val;
   uint32_t    count;
+  uint32_t    nnormalvals;
   uint32_t    nvals;
   uint64_t    i;
+  uint32_t    dims[4];
   args_t      args;
+  uint8_t     passed;
   struct argp argp = {options, _parse_opt, "INPUT", doc}; 
 
-  count     = 0;
-  threshold = 0;
-  data      = NULL;
+
+  nnormalvals = 0;
+  count       = 0;
+  data        = NULL;
+
+  memset(&args, 0, sizeof(args));
 
   startup("countimg", argc, argv, &argp, &args);
 
@@ -89,20 +99,39 @@ int main(int argc, char *argv[]) {
 
   for (i = 0; i < nvals; i++) {
 
-    val = analyze_read_by_idx(&hdr, data, i);
+    passed = 0;
+    val    = analyze_read_by_idx(&hdr, data, i);
+
+    if (!isnormal(val))
+      continue;
+
+    nnormalvals ++;
 
     if (args.absolute)
       val = abs(val);
 
     if (args.lessthan) {
-      if (val <= args.threshold) count++;
+      if (val <= args.threshold) passed = 1;
     }
     else {
-      if (val >= args.threshold) count++;
+      if (val >= args.threshold) passed = 1;
+    }
+
+    if (!passed) continue;
+
+    count ++;
+    if (args.printidx) {
+      analyze_get_indices(&hdr, i, dims);
+      printf("%2u %2u %2u: %6.4f\n" , dims[0], dims[1], dims[2], val);
     }
   }
 
-  printf("%u / %u values above %0.3f\n", count, nvals, threshold);
+  printf(
+    "%u / %u values %s %0.3f\n",
+    count,
+    nnormalvals,
+    args.lessthan ? "below" : "above",
+    args.threshold);
 
   free(data);
   exit(0);
